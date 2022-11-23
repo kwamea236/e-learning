5a8f51a99b3dfbcc1875d1dc7c19bc23b4b655f3
kwamea236 <kwamea236@protonmail.com>
2022-11-23 03:36:32 +0000

    render 13

--- dist/App.js
+++ dist/App.js
@@ -1,12 +1,14 @@
-import express from "express";
-
-const app = express();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const PORT = process.env.PORT;
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to my component</h1>");
-})
-
-
-app.listen(PORT);
+app.get("/", (req, res) => {
+    res.send("Welcom you are working");
+});
+app.listen(PORT, () => {
+    console.log("server is running");
+});
---
+++ src/App2.ts
@@ -0,0 +1,58 @@
+/**
+ * Author: Kwame Ato
+ * Date: 18th November, 2022
+ */
+
+import express, { Express, Request, Response } from "express";
+import connectDB from "./ConnectDB.js";
+import { StatusCodes } from "http-status-codes";
+import bodyParser from "body-parser"
+import { deleteUser, getAllUsers, getUserById, postUser, updateUser } from "./Handlers.js";
+import cors from "cors";
+ 
+
+// connect database message
+connectDB();
+
+const app: Express = express();
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+const PORT = process.env.PORT;
+
+app.get("/api",getAllUsers)
+
+app.get("/api/:id",getUserById)
+
+app.post("/api",postUser)
+
+app.put("/api/:id", updateUser)
+
+app.delete("/api/:id",deleteUser)
+
+
+
+/**
+ * Custom error page
+ */
+
+app.use((req: Request, res: Response)=>{
+    res.status(StatusCodes.NOT_FOUND);
+    res.send("404 page not found");
+})
+
+app.use((req: Request, res: Response)=>{
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.send("INTERNAL SERVER ERROR");
+})
+
+
+//server
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}\nPress Ctl + C to terminate server`);
+})
